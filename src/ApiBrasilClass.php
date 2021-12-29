f3fe51c0310ce897aa0ef0c9b24399c157044585
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2021-12-29 00:16:56 -0300

    example return function handler

--- src/ApiBrasilClass.php
+++ src/ApiBrasilClass.php
@@ -1,14 +1,11 @@
 <?php
 namespace ApiGratis;
-
-use ApiGratis\Base;
-
 class ApiBrasil extends Base
 {
 
     public function handler()
     {
-        echo 'Bem vindo ao APIBrasil.';
+        return 'Bem vindo ao APIBrasil.';
     }
 
 
